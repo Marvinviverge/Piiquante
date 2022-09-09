24e8ed92e28d2b6ea332d6acde2c5b8d9ff0b6bf
Marvin Viverge <marvin.viverge@gmail.com>
2022-09-09 16:15:15 +0200

    sauce models

---
+++ BackEnd/controllers/sauce.controller.js
@@ -0,0 +1,29 @@
+const User = require("../models/sauce.models");
+
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
+const dotenv = require("dotenv").config({ encoding: "latin1" });
+
+exports.getAllSauce = (req, res, next) => {
+
+}
+
+exports.getOneSauce = (req, res, next) => {
+
+}
+
+exports.createSauce = (req, res, next) => {
+
+}
+
+exports.modifySauce = (req, res, next) => {
+
+}
+
+exports.deleteSauce = (req, res, next) => {
+
+}
+
+exports.likeDislikeSauce = (req, res, next) => {
+
+}
